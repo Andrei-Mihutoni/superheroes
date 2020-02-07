90705b4980ddfaa1fc4ff6eb9875ca9990db9dd6
Andrei-Mihutoni <andrei.mihutoni@gmail.com>
2020-02-07 10:53:07 +0100

    take 1

---
+++ forEach.js
@@ -0,0 +1,18 @@
+const animalsforAdoption = [
+   { species = "cat",
+    age = 23,
+    }
+    species = "cat",
+    age = 23,
+]
+
+
+animalsforAdoption.forEach(sayHi)
+
+function sayHi() {
+    console.log(`Hi there, meet ${species} he's ${age} years old`)
+
+}
+
+
+
